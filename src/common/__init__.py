0d28f10762c3a6adc22f489d8a9457e976ca5ed5
lichao <13259988689@163.com>
2024-06-18 23:05:38 +0800

    更新项目信息 2024年6月18日23:05:34

---
+++ src/common/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+# @File: __init__.py.py
+# @Author: lichao
+# @Time: 2024-06-18 22:21
+# @Software: PyCharm
---
+++ src/demo.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+# @File: demo.py
+# @Author: lichao
+# @Time: 2024-06-18 22:21
+# @Software: PyCharm
+
+
